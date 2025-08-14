31308078c480943161002ea0ab0d6cd1fc30d246
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-08-14 11:30:32 -0700

    adds all-panos argument to DockerEntrypoint script

--- DownloadRunnerDockerEntrypoint.sh
+++ DownloadRunnerDockerEntrypoint.sh
@@ -4,11 +4,20 @@
 
 mkdir -p /tmp/download_dest
 chmod 600 /app/id_rsa
+
+# Check if last argument is --all-panos
+all_panos=""
+args=("$@")
+if [[ "${@: -1}" == "--all-panos" ]]; then
+    all_panos="--all-panos"
+    # Remove last argument
+    set -- "${@:1:$(($#-1))}"
+fi
 if [ $# -eq 1 ]; then
-    python3 DownloadRunner.py $1 /tmp/download_dest
+    python3 DownloadRunner.py $1 /tmp/download_dest $all_panos
 elif [ $# -eq 3 ]; then
     echo "Mounting $2 port $3 for $1"
-    sshfs -o IdentityFile=/app/id_rsa,StrictHostKeyChecking=no $2 /tmp/download_dest -p $3 && python3 DownloadRunner.py $1 /tmp/download_dest; umount /tmp/download_dest
+    sshfs -o IdentityFile=/app/id_rsa,StrictHostKeyChecking=no $2 /tmp/download_dest -p $3 && python3 DownloadRunner.py $1 /tmp/download_dest $all_panos; umount /tmp/download_dest
 else
     echo "Usage:"
     echo "  ./DownloadRunnerDockerEntrypoint sidewalk_server_fqdn"
